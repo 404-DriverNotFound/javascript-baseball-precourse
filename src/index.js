80dff4981c086f89931e29b3910deac268416314
hyo-choi <hyochoi31@gmail.com>
2021-06-14 20:13:54 +0900

    feat: submit 이벤트 처리

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 export default function BaseballGame() {
   let answer;
 
+  const userInput = document.querySelector('#user-input');
+  const submitButton = document.querySelector('#submit');
+
   this.getRandomDecimalLessThan = function getRandomDecimalLessThan(num) {
     return Math.floor((Math.random() * 10) % num);
   };
 
-  this.setRandomAnswer = function setRandomAnswer() {
+  this.setRandomAnswer = () => {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     answer = 0;
     for (let i = 0; i < 3; i += 1) {
@@ -16,9 +19,20 @@ export default function BaseballGame() {
     return answer;
   };
 
+  this.handleSubmit = function handleSubmit() {
+    this.play(answer, +(userInput.value));
+    // TODO: 힌트 생성 및 정답 처리
+  };
+
   this.play = function play(computerInputNumbers, userInputNumbers) {
+  // TODO: 유효성 검사
     return "결과 값 String";
   };
+
+  const handler = this.handleSubmit.bind(this);
+
+  this.setRandomAnswer();
+  submitButton.addEventListener('click', handler);
 }
 
 new BaseballGame();
