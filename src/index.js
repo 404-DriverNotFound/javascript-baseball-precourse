cb447a0ac294584817d24f34353011a6dbaceb36
hyo-choi <hyochoi31@gmail.com>
2021-06-14 19:26:41 +0900

    feat: 컴퓨터 측 정답 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+  let answer;
+
+  this.getRandomDecimalLessThan = function getRandomDecimalLessThan(num) {
+    return Math.floor((Math.random() * 10) % num);
+  };
+
+  this.setRandomAnswer = function setRandomAnswer() {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    answer = 0;
+    for (let i = 0; i < 3; i += 1) {
+      const idx = this.getRandomDecimalLessThan(array.length);
+      answer += array[idx] * (10 ** i);
+      array.splice(idx, 1);
+    }
+    return answer;
+  };
+
+  this.play = function play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
