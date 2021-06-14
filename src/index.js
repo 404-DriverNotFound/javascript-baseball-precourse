a7003b8047829df101d8937a94c5ce96e07e31cf
hyo-choi <hyochoi31@gmail.com>
2021-06-14 21:28:04 +0900

    feat: 힌트 생성 기능

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ export default function BaseballGame() {
   this.handleError = function handleError() {
     userInput.value = '';
     alert('잘못된 값을 입력하셨습니다.\n1부터 9까지의 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요.\n예시) 123, 645, 987');
+    return false;
   };
 
   this.isValidInput = function isValidInput(input) {
@@ -48,16 +49,60 @@ export default function BaseballGame() {
     return true;
   };
 
+  this.makeGameResult = function makeGameResult(input) {
+    const inputArray = [];
+    const answerArray = answer.slice();
+    const obj = {
+      balls: 0,
+      strikes: 0,
+    };
+    let num = input;
+
+    for (let i = 0; i < 3; i += 1) {
+      inputArray.splice(0, 0, num % 10);
+      num = Math.floor(num / 10);
+    }
+
+    for (let i = 0; i < inputArray.length; i += 1) {
+      if (inputArray[i] === answerArray[i]) {
+        obj.strikes += 1;
+        inputArray.splice(i, 1);
+        answerArray.splice(i, 1);
+        i -= 1;
+      }
+    }
+    for (let i = 0; i < inputArray.length; i += 1) {
+      obj.balls += (answerArray.includes(inputArray[i]) ? 1 : 0);
+    }
+    return obj;
+  };
+
   this.play = function play(computerInputNumbers, userInputNumbers) {
     if (!this.isValidInput(userInputNumbers)) {
       return this.handleError();
     }
-    // TODO: 힌트 생성
-    return "결과 값 String";
+    const gameResult = this.makeGameResult(userInputNumbers);
+    let resultStr = '';
+    if (gameResult.balls > 0) {
+      resultStr += `${gameResult.balls}볼`;
+    }
+    if (gameResult.balls > 0 && gameResult.strikes > 0) {
+      resultStr += ' ';
+    }
+    if (gameResult.strikes > 0) {
+      resultStr += `${gameResult.strikes}스트라이크`;
+    }
+    if (resultStr.length === 0) {
+      return '낫싱';
+    }
+    return resultStr;
   };
 
   this.handleSubmit = function handleSubmit() {
     const result = this.play(answer, +(userInput.value));
+    if (result === false) {
+      return;
+    }
     // TODO: 정답 처리
   };
 
