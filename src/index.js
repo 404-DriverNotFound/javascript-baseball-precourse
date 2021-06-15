68cd1c7c90546935ed2a4382027f4ef85001ce75
Sungwoo Park <codest99@gmail.com>
2021-06-15 22:25:06 +0900

    chore: 사용자가 채운 input 값을 가져오는 기능 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import { pickRandomNumbers } from './utils.js';
 export default function BaseballGame() {
   const answer = pickRandomNumbers();
 
+  const userInput = document.getElementById("user-input");
+  const submitButton = document.getElementById("submit");
+
   this.play = function baseballPlay(computerInputNumbers, userInputNumbers) {
+    console.log(userInputNumbers);
     return '결과 값 String';
   };
 
-  console.log(answer);
+  submitButton.addEventListener("click", () => {
+    this.play(answer, userInput.value);
+  });
 }
 
 // eslint-disable-next-line no-new
