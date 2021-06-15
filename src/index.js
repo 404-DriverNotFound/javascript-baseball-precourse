deae84a224e96a00b70e4662bf4eb26954fb8b1f
Ykoh <ykoh@student.42seoul.kr>
2021-06-15 20:54:02 +0900

    Feat: userInputNumbers를 만드는 기능 추가
    * Feat: implement isValidUserInputNumbers function
    
    * Feat: getUserInputNumbers function
    
    * Feat: addEventListener to button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import getUserInputNumbers from './getUserInputNumbers.js';
+
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', () => {
+  const userInputNumbers = getUserInputNumbers();
+  alert(userInputNumbers);
+});
+
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+  this.play = function play(computerInputNumbers, userInputNumbers) {
+    return '결과 값 String';
   };
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+BaseballGame();
